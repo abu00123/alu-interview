97d52b7fb20f4de0df3e54582d7fc042e2c9c8d6
abu00123 <a.garang@alustudent.com>
2025-03-14 08:57:30 -0700

    work

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+def pascal_triangle(n):
+    """Returns a list of lists representing Pascal's Triangle of n."""
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]  # Initialize with the first row
+    
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]  # First element is always 1
+        
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        
+        new_row.append(1)  # Last element is always 1
+        triangle.append(new_row)
+    
+    return triangle
+
+# Example usage:
+n = 5
+for row in pascal_triangle(n):
+    print(row)
+
